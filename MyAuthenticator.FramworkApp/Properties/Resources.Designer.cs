3ea36fa6c9b369ac9eec6911a9180e54d6c9b5d9
Hamed Movaghari <hmovaghari@gmail.com>
2024-06-20 10:09:19 +0330

    Version 1.0.2

--- MyAuthenticator.FramworkApp/Properties/Resources.Designer.cs
+++ MyAuthenticator.FramworkApp/Properties/Resources.Designer.cs
@@ -361,7 +361,7 @@ namespace MyAuthenticator.FramworkApp.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.0.1.
+        ///   Looks up a localized string similar to 1.0.2.
         /// </summary>
         internal static string Version {
             get {
